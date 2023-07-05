7651cdc3d9bd438fda1b386713451008d3be17ac
dev-oe <deliicmugambi123@gmail.com>
2023-07-05 17:21:40 +0300

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,8 +4,15 @@
  * @s: pointer to the block of memory to fill
  * @b: constant byte
  * @n: the number of bytes to be filled
+ * Return: a pointer to the filed memory area
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-	return (memset(s, b, n));
+	unsigned int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
